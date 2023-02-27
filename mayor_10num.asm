cc28ab725ca5c6ca85dbaa40ea4d7aad2368b5c7
albertocrespop <104109660+albertocrespop@users.noreply.github.com>
2023-02-27 18:15:48 +0100

    Enunciado Ejercicio 2/Práctica 3
    2.- Implemente un programa en ensamblador de MIPS que contenga las siguientes
        funciones y procedimientos:
    
      - mayor: recibe dos números enteros y devuelve el mayor de ellos
    
      - mostrar_cadena: muestra por pantalla la cadena cuya dirección de memoria se
        pasa como parámetro.
    
      - mostrar_numero: muestra por pantalla el número cuyo valor se
        pasa como parámetro.
    
      - pedir_numero: recibe como parámetro un número, que muestra seguido del
        carácter dos puntos (:) para pedir un número entero a continuación. Como resultado,
        devuelve el número introducido. Por ejemplo, si esta función recibe 5,
        mostrará por pantalla «5: » y pedirá un número a continuación.
    
      Usando las funciones anteriores, la función main de nuestro programa debe
      entrar en un bucle donde se pidan 10 números y, al final, muestre el mayor de
      todos ellos. A continuación se muestra un ejemplo de ejecución:
    
      Introduce 10 números enteros:
      1: -34
      2: 134
      3: 834
      4: -562
      5: 341
      6: 623
      7: 621
      8: 891
      9: 15
      10: -3411
      El mayor de los números introducidos es 891

---
+++ mayor_10num.asm
@@ -0,0 +1,85 @@
+	.data
+dospuntos:	
+	.asciiz		": "
+mostrarmayor:
+	.asciiz		"\nEl número mayor es: "
+	.text
+mayor:				# $a0, $a1 - Entero a y b
+	bgt 	$a0,$a1,a0
+	move	$v0, $a1
+	jr	$ra
+a0:	
+	move	$v0, $a0
+	jr	$ra
+	
+mostrar_cadena:			# $a0 = dirección de cadena
+	li	$v0, 4
+	syscall
+	jr	$ra
+mostrar_numero:			# $a0 = entero por mostrar
+	li	$v0, 1
+	syscall
+	jr	$ra
+pedir_numero:			# $a0 = direccion de cadena "pedirnumx"
+	addiu	$sp, $sp, -4	# Apila $ra
+	sw	$ra, 0($sp)
+	
+	jal	mostrar_cadena
+	
+	li	$v0, 5
+	syscall
+	
+	lw	$ra, 0($sp)	# Desapila
+	addiu	$sp, $sp, 4
+	
+	jr	$ra
+	
+	.globl main
+main:
+	addiu	$sp, $sp, -16
+	lw	$s0, 0($sp)
+	lw	$s1, 4($sp)
+	lw	$s2, 8($sp)
+	lw	$ra, 12($sp)
+	
+	li	$s0, 1		# $s0 = i
+	
+	move	$a0, $s0
+	jal	mostrar_numero
+	la	$a0, dospuntos
+	jal	pedir_numero
+	move	$s1, $v0	# $s1 = mayor
+	
+	addiu	$s0, $s0, 1
+bucle:
+	bgt	$s0,10,finbucle
+	move	$a0, $s0
+	jal	mostrar_numero
+	la	$a0, dospuntos
+	jal	pedir_numero
+	move	$s2, $v0	# $s2 = comparar
+	
+	move	$a0, $s1
+	move	$a1, $s2
+	jal	mayor
+	move	$s1, $v0
+	
+	addiu	$s0, $s0, 1
+
+	j	bucle
+finbucle:
+	la	$a0, mostrarmayor
+	li	$v0, 4
+	syscall
+	
+	li	$v0, 1
+	move	$a0, $s1
+	syscall
+
+	sw	$s0, 0($sp)
+	sw	$s1, 4($sp)
+	sw	$s2, 8($sp)
+	sw	$ra, 12($sp)
+	li	$v0, 10
+	syscall
+	jr	$ra
